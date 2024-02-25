f6b31392f72a2c347b2409d428e77da7755c43d6
shubham <shubhamnainwal70@gmail.com>
2024-02-25 12:46:02 +0530

    middleware

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,24 @@
+import { ApiError } from "../utils/ApiError";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer", "");
+    if (!token) {
+      throw new ApiError(401, "Unauthoize request");
+    }
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const user = await User.findById(decodedToken?._id).select(
+      "-password-refershToken"
+    );
+
+    if (!user) {
+      throw new ApiError(401, "Invalid Access  Token");
+    }
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid access token");
+  }
+});
